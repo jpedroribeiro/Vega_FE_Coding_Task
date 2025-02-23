42ce1d811334256d4bfd63fac1dc7a43f1ef159f
J. Pedro Ribeiro <1715622+jpedroribeiro@users.noreply.github.com>
2025-02-23 16:03:24 +0000

    added history, matched all data points to same source

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import type { Route } from "./+types/home";
 import logo from "/public/logo.jpg";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import {
+  Chart as ChartJS, ArcElement, Tooltip, Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from 'chart.js';
+import { Doughnut, Line } from 'react-chartjs-2';
 
 interface Asset {
   label: string;
   quantity: number;
   type: string;
   price: number;
+  date: string;
 }
 
 interface DonutChartData {
@@ -22,12 +29,27 @@ interface DonutChartData {
   }[];
 }
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend
+);
 
 export async function clientLoader({
   params,
 }: any) {
-  /* TODO credentials and API with more data? */
+  /* 
+    TODO
+      - credentials and API with more data?
+      - filtering date and type could be optimised
+      - more types (charts etc)
+  
+  */
+
 
   // CREDENTIALS: load email and visits from localStorage
   const email = localStorage.getItem("email");
@@ -41,48 +63,59 @@ export async function clientLoader({
   return { email, visits, assetData };
 }
 
-export default function DashboardRoute({ loaderData }: any) {
+export default function DashboardRoute({ loaderData }: { loaderData: { email: string, visits: string, assetData: { donut: Asset[], donutAndHistoricalData: Asset[] } } }) {
   const ALL_TYPES = "all-assets";
-  const [selectedType, setSelectedType] = React.useState<string>(ALL_TYPES);
+  const COLOURS = {
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.92)',
+      'rgba(54, 162, 235, 0.92)',
+      'rgba(255, 206, 86, 0.92)',
+      'rgba(75, 192, 192, 0.92)',
+      'rgba(153, 102, 255, 0.92)',
+      'rgba(255, 159, 64, 0.92)',
+    ],
+    borderColor: [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+    ],
+  }
   const { email, visits, assetData } = loaderData;
+  const [selectedType, setSelectedType] = React.useState<string>(ALL_TYPES);
+  const [selectedDate, setSelectedDate] = React.useState<string>(assetData.donutAndHistoricalData.sort((a: Asset, b: Asset) => new Date(b.date).getTime() - new Date(a.date).getTime())[0].date);
   const [donutData, setDonutData] = React.useState<DonutChartData | null>(null);
   const availableAssetsTypes: string[] = [... new Set<string>(assetData.donut.map((asset: Asset) => asset.type))];
   const [tableData, setTableData] = React.useState<any[]>([]);
+  const [lineData, setLineData] = React.useState<any>(null);
 
   function handleDonutDataChange(event: React.ChangeEvent<HTMLSelectElement>) {
     const assetLabel = event.target.value;
     setSelectedType(assetLabel);
   }
 
+  function handleDateChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const date = event.target.value;
+    setSelectedDate(date);
+  }
+
   function updateDonutData() {
-    // Create specific data containers based on user data (from API)
+    // Donut#1 - All assets by type
     const donut_1: Record<string, number> = {};
-    assetData.donut.forEach((asset: Asset) => {
+    assetData.donutAndHistoricalData.filter((asset: Asset) => asset.date === selectedDate).forEach((asset: Asset) => {
       donut_1[asset.type] ? donut_1[asset.type] += asset.quantity * asset.price : donut_1[asset.type] = asset.quantity * asset.price;
     });
 
     const defaultDonut: DonutChartData = {
-      labels: Object.keys(donut_1),
+      labels: Object.keys(donut_1).map((type: string) => `${type[0].toUpperCase()}${type.slice(1)}`),
       datasets: [
         {
           label: 'Total value in USD$',
           data: Object.values(donut_1),
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.92)',
-            'rgba(54, 162, 235, 0.92)',
-            'rgba(255, 206, 86, 0.92)',
-            'rgba(75, 192, 192, 0.92)',
-            'rgba(153, 102, 255, 0.92)',
-            'rgba(255, 159, 64, 0.92)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
+          backgroundColor: COLOURS.backgroundColor,
+          borderColor: COLOURS.borderColor,
           borderWidth: 1,
         },
       ],
@@ -92,8 +125,9 @@ export default function DashboardRoute({ loaderData }: any) {
     if (selectedType === ALL_TYPES) {
       setDonutData(defaultDonut);
     } else {
+      // Donut#2 - Individual assets of a specific type
       const donut_2 = {} as Record<string, number>;
-      assetData.donut.forEach((asset: Asset) => {
+      assetData.donutAndHistoricalData.filter((asset: Asset) => asset.date === selectedDate).forEach((asset: Asset) => {
         if (asset.type === selectedType) donut_2[asset.label] ? donut_2[asset.label] += asset.quantity * asset.price : donut_2[asset.label] = asset.quantity * asset.price;
       });
 
@@ -113,62 +147,128 @@ export default function DashboardRoute({ loaderData }: any) {
 
   function updateTableData() {
     setTableData(
-      assetData.donut.filter((asset: Asset) => selectedType === ALL_TYPES || asset.type === selectedType)
+      assetData.donutAndHistoricalData
+        .filter((asset: Asset) => selectedType === ALL_TYPES || asset.type === selectedType)
+        .filter((asset: Asset) => asset.date === selectedDate)
     )
+  }
+
+
+  function updateLineData() {
+    const options = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top' as const,
+        }
+      },
+    };
+
+    const filteredAssetsData: Asset[] = assetData.donutAndHistoricalData
+      .filter((asset: Asset) => new Date(asset.date) <= new Date(selectedDate) && (asset.type === selectedType || selectedType === ALL_TYPES))
+      .sort((a: Asset, b: Asset) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    const labels = [...new Set(filteredAssetsData.map((asset: Asset) => asset.date))];
+    const assetTypes = [...new Set(filteredAssetsData.map((asset: Asset) => selectedType === ALL_TYPES ? asset.type : asset.label))];
+
+
+
+    const data = {
+      labels,
+      datasets: [
+        ...assetTypes.map((type: string, index: number) => {
+          const label = `${type[0].toUpperCase()}${type.slice(1)}`;
+          const entryData: { [key: string]: number } = {};
+          filteredAssetsData.filter((asset: Asset) => asset[selectedType === ALL_TYPES ? 'type' : 'label'] === type).forEach((asset: Asset) => {
+            entryData[asset.date] = (entryData[asset.date] || 0) + (asset.price * asset.quantity);
+          })
+
+          return {
+            label,
+            data: Object.values(entryData),
+            borderColor: COLOURS.borderColor[index % COLOURS.borderColor.length],
+            backgroundColor: COLOURS.backgroundColor[index % COLOURS.backgroundColor.length],
+          }
+        }),
+      ],
+    };
+
+
+    setLineData({
+      options,
+      data
+    })
+
+
   }
 
   React.useEffect(() => {
     updateDonutData();
     updateTableData();
-  }, [selectedType]);
+    updateLineData();
+  }, [selectedType, selectedDate]);
 
   return (
-    <main className="flex flex-col items-center justify-center gap-4 m-[24px]">
-      <header className="flex flex-col items-center gap-9">
-        <img
-          src={logo}
-          alt="VEGA"
-          className="block"
-          width={100}
-          height={100}
-        />
-      </header>
-      <div className="space-y-6 px-4">
-        <nav className="space-y-4">
-          <p className="leading-6 text-gray-200 text-center pb-2 text-sm">
-            Welcome back, {email || "TODO"}, you have visited the dashboard {visits || "TODO"} times.
-          </p>
-        </nav>
+    <main className="max-w-[600px] mx-auto flex flex-col items-center justify-center gap-4 m-[24px] relative">
+      <div className="w-full sticky top-0 left-0 right-0 bg-gray-950/90 pb-4">
+        <header className="flex flex-col items-center gap-9">
+          <img
+            src={logo}
+            alt="VEGA"
+            className="block"
+            width={100}
+            height={100}
+          />
+        </header>
+        <div className="space-y-6 px-4">
+          <nav className="space-y-4">
+            <p className="leading-6 text-gray-200 text-center pb-2 text-sm">
+              Welcome back, {email || "TODO"}, you have visited the dashboard {visits || "TODO"} times.
+            </p>
+          </nav>
+        </div>
+        <div className="w-full flex gap-4 justify-between">
+          <select className="flex-1 basis-1/2 p-2 rounded-md border border-gray-700"
+            onChange={handleDonutDataChange}
+          >
+            <optgroup label="All available assets">
+              <option defaultChecked value={ALL_TYPES}>Total by type</option>
+            </optgroup>
+            <optgroup label="Individual asset type">
+              {
+                availableAssetsTypes.sort().map((label: string, index: number) => {
+                  return <option key={index} value={label}>{label.charAt(0).toUpperCase() + label.slice(1)}</option>
+                })
+              }
+            </optgroup>
+          </select>
+
+          <select className="flex-1 basis-1/2 p-2 rounded-md border border-gray-700"
+            onChange={handleDateChange}
+          >
+            {[...new Set(assetData.donutAndHistoricalData
+              .map((asset: Asset) => asset.date)
+              .filter((date): date is string => date !== undefined)
+              .sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
+            )].map((date: string, index: number) => <option key={index} value={date}>{date}</option>)}
+          </select>
+
+        </div>
       </div>
       <div className="flex flex-col items-center w-full rounded-3xl border p-6 border-gray-700 space-y-4 gap-4">
         {/*
           Task 1: Donut chart with total assets by type
         */}
         <h2 className="w-full text-lg text-gray-200 font-semibold leading-6 text-center border-b border-gray-600 pb-2">❶ Available assets</h2>
-        <div className="w-full max-w-[600px]">
+        <div className="w-full max-w-[600px] mb-20">
           {donutData && <Doughnut data={donutData} />}
         </div>
-        <select className="w-full max-w-[600px] p-2 rounded-md border border-gray-700 mb-20"
-          onChange={handleDonutDataChange}
-        >
-          <optgroup label="All available assets">
-            <option defaultChecked value={ALL_TYPES}>Total by type</option>
-          </optgroup>
-          <optgroup label="Individual asset type">
-            {
-              availableAssetsTypes.sort().map((label: string, index: number) => {
-                return <option key={index} value={label}>{label.charAt(0).toUpperCase() + label.slice(1)}</option>
-              })
-            }
-          </optgroup>
-        </select>
 
         {/*
           Task 2: Table with your positions
         */}
         <h2 className="w-full text-lg text-gray-200 font-semibold leading-6 text-center border-b border-gray-600 pb-2">❷ Your Positions</h2>
         <div className="w-full max-w-[600px]">
-          <table className="w-full border-t border-gray-700 text-white">
+          <table className="w-full border-t border-gray-700 text-white mb-20">
             <thead>
               <tr className="bg-gray-800 text-left">
                 <th className="p-3 border-b border-gray-700">Asset</th>
@@ -218,8 +318,8 @@ export default function DashboardRoute({ loaderData }: any) {
           Task 3: Historical chart portfolio over time
         */}
         <h2 className="w-full text-lg text-gray-200 font-semibold leading-6 text-center border-b border-gray-600 pb-2">❸ Portfolio Through Time</h2>
-        <div className="w-full max-w-[600px]">
-
+        <div className="w-full max-w-[600px] mb-10">
+          {lineData && <Line options={lineData.options} data={lineData.data} />}
         </div>
 
       </div>
